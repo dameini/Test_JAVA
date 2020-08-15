6f968d4c50b6b064b940e08e65e65150aac33357
dameini <1029177123@qq.com>
2020-08-15 21:54:32 +0800

    0.4

--- src/Test_Package/Test_Class.java
+++ src/Test_Package/Test_Class.java
@@ -1,8 +1,25 @@
 package Test_Package;
 
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.Scanner;
+
 public class Test_Class {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("java");
+
+        double randNum = Math.random();// 生成一个随机数
+        System.out.println(randNum);
+
+        System.out.println("输入");
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        System.out.println(str);
+
+        int[] intArray = new int[6];
+        intArray[0] = 2;
+        java.util.OptionalInt c = Arrays.stream(intArray).max();
+        System.out.println(Arrays.stream(intArray).max());
+
     }
 }
-
