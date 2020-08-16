37189b56fdca7fd075ab128ece38ce608d931275
dameini <1029177123@qq.com>
2020-08-16 13:34:17 +0800

    0.6

---
+++ src/ReferenceNumType.java
@@ -0,0 +1,33 @@
+public class ReferenceNumType {
+    public static void main(String[] args) {
+        // TODO 类是对象的模板，对象是类的实例
+        // TODO 一个Java类不能重名
+        // TODO 一个类可以有很多对象，对象只能根据一个类来创建
+        //
+        // TODO 引用必须是、只能是一个类的引用
+        // TODO 引用只能指向其所属类型的类的对象
+        // TODO 相同类型的引用之间可以赋值
+        // TODO 只能通过指向一个对象的引用，来操作一个对象
+        classs c1;
+        c1 = new classs();
+        System.out.println(c1);
+// TODO 数组其实也是一个引用数据类型
+        int[] shuzu;
+        shuzu = new int[2];
+        shuzu[0] = 1;
+        shuzu[1] = 2;
+        System.out.println(shuzu.length);
+
+        double[][][] sanwei = new double[1][2][3];
+
+        System.out.println(sanwei[0][1].length);
+        // TODO 引用的缺省值null
+        if (c1.id == null)
+            System.out.println("ss");
+        for (int x:
+             shuzu) {
+            System.out.println(x);
+        }
+        // 视频5.55
+    }
+}
--- src/Test_Package/TestJava.java
+++ src/Test_Package/TestJava.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.OptionalInt;
 import java.util.Scanner;
 
-public class Test_Class {
+public class TestJava {
     public static void main(String[] args) {
         System.out.println("java");
 
